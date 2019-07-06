c0b77d2a3da07f1add0a20ff8eb9ad7064e5f661
Nicholas_Bingei <nbinged@gmail.com>
2019-07-06 23:41:25 +0800

    2nd Commit:
    Added CSS design.
    Card images.
    Some DOM manipulation code.

---
+++ project-1/cards.js
@@ -0,0 +1,162 @@
+//////////////////////////////////////
+///Object of all the various cards:///
+//////////////////////////////////////
+
+var allcards = [
+{
+rank: "ace",
+suit: "hearts",
+cardImage: "images/hearts/hearts-ace.png"
+},
+
+{
+rank: "2",
+suit: "hearts",
+cardImage: "images/hearts/hearts-2.png"
+},
+
+{
+rank: "3",
+suit: "hearts",
+cardImage: "images/hearts/hearts-3.png"
+},
+
+{
+rank: "4",
+suit: "hearts",
+cardImage: "images/hearts/hearts-4.png"
+},
+
+{
+rank: "5",
+suit: "hearts",
+cardImage: "images/hearts/hearts-5.png"
+},
+
+{
+rank: "6",
+suit: "hearts",
+cardImage: "images/hearts/hearts-6.png"
+},
+
+{
+rank: "7",
+suit: "hearts",
+cardImage: "images/hearts/hearts-7.png"
+},
+
+{
+rank: "8",
+suit: "hearts",
+cardImage: "images/hearts/hearts-8.png"
+},
+
+{
+rank: "9",
+suit: "hearts",
+cardImage: "images/hearts/hearts-9.png"
+},
+
+{
+rank: "10",
+suit: "hearts",
+cardImage: "images/hearts/hearts-10.png"
+},
+
+{
+rank: "jack",
+suit: "hearts",
+cardImage: "images/hearts/hearts-jack.png"
+},
+
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/hearts/hearts-queen.png"
+},
+
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/hearts/hearts-king.png"
+}
+];
+
+/////
+{
+rank: "ace",
+suit: "hearts",
+cardImage: "images/hearts/hearts-ace.png"
+},
+
+{
+rank: "2",
+suit: "hearts",
+cardImage: "images/hearts/hearts-2.png"
+},
+
+{
+rank: "3",
+suit: "hearts",
+cardImage: "images/hearts/hearts-3.png"
+},
+
+{
+rank: "4",
+suit: "hearts",
+cardImage: "images/hearts/hearts-4.png"
+},
+
+{
+rank: "5",
+suit: "hearts",
+cardImage: "images/hearts/hearts-5.png"
+},
+
+{
+rank: "6",
+suit: "hearts",
+cardImage: "images/hearts/hearts-6.png"
+},
+
+{
+rank: "7",
+suit: "hearts",
+cardImage: "images/hearts/hearts-7.png"
+},
+
+{
+rank: "8",
+suit: "hearts",
+cardImage: "images/hearts/hearts-8.png"
+},
+
+{
+rank: "9",
+suit: "hearts",
+cardImage: "images/hearts/hearts-9.png"
+},
+
+{
+rank: "10",
+suit: "hearts",
+cardImage: "images/hearts/hearts-10.png"
+},
+
+{
+rank: "jack",
+suit: "hearts",
+cardImage: "images/hearts/hearts-jack.png"
+},
+
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/hearts/hearts-queen.png"
+},
+
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/hearts/hearts-king.png"
+}
--- project-1/script.js
+++ project-1/script.js
@@ -1,51 +1,119 @@
 console.log("Hello script.js")
-//Global Variables:
+/////////////////////
+//Global Variables://
+/////////////////////
 
-//Deck of 52 cards. 4 suits, Spade, Clubs, Hearts and Diamonds. 13 of each.
-var cardSuits = ["spades", "hearts", "diamonds", "clubs"];
-var cards = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
-var cardValues = ["2", "3", "4", "5", "6", "7","8","9","10","10","10","10","11"]; // Currently 1 suit =. 1 cards
-var cardDeck = [];
-var playerCards = [];
+var allcards = [
+{
+rank: "ace",
+suit: "hearts",
+cardImage: "images/Hearts-Suit/hearts-ace.png"
+},
+
+{
+rank: "2",
+suit: "hearts",
+cardImage: "images/Hearts-Suit/hearts-2.png"
+},
+
+{
+rank: "3",
+suit: "hearts",
+cardImage: "images/Hearts-Suit/hearts-3.png"
+},
+
+{
+rank: "4",
+suit: "hearts",
+cardImage: "images/Hearts-Suit/hearts-4.png"
+},
+
+{
+rank: "5",
+suit: "hearts",
+cardImage: "images/Hearts-Suit/hearts-5.png"
+},
+
+{
+rank: "6",
+suit: "hearts",
+cardImage: "images/Hearts-Suit/hearts-6.png"
+},
+
+{
+rank: "7",
+suit: "hearts",
+cardImage: "images/Hearts-Suit/hearts-7.png"
+},
+
+{
+rank: "8",
+suit: "hearts",
+cardImage: "images/Hearts-Suit/hearts-8.png"
+},
+
+{
+rank: "9",
+suit: "hearts",
+cardImage: "images/Hearts-Suit/hearts-9.png"
+},
+
+{
+rank: "10",
+suit: "hearts",
+cardImage: "images/Hearts-Suit/hearts-10.png"
+},
+
+{
+rank: "jack",
+suit: "hearts",
+cardImage: "images/Hearts-Suit/hearts-jack.png"
+},
+
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/Hearts-Suit/hearts-queen.png"
+},
+
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/Hearts-Suit/hearts-king.png"
+}
+];
+
+// var cardSuits = ["spades", "hearts", "diamonds", "clubs"];
+var cardValues = ["2", "3", "4", "5", "6", "7","8","9","10","J","Q","K","A"];
+var randomCard = (Math.floor(Math.random() * cardValues.length));
+var playersCards = [];
 var sumOfPlayersCards = 0;
-var index = playerCards
+
+/////////////////////////////
+//Upon loading the web-page//
+/////////////////////////////
+
+window.onload = function(){
+    createBoard()
+    console.log("Game board loaded")
+};
 
 /////////////////////
 //Starting the game//
 /////////////////////
-
-// // Show the Start button
-// var showStartBtn = function(){
-//   var startButton = document.querySelector('#start');
-//   startButton.classList.remove('hidden');
-//   console.log("show button");
-// };
-
-// // Hide the Start button
-// var hideStartBtn = function(){
-//   var startButton = document.querySelector('#start');
-//   startButton.classList.add('hidden');
-//   console.log("hide button");
-// };
-
-// When player clicks start game, 2 cards will appear. with "Stay" and "Hit Me button"
-var randomCard = (Math.floor(Math.random() * cardValues.length));
-// if (check duplicate cards) {}
-
 var startGame = function(event) {
     var randomCard = "";
-    // console.log("created board");
-    // hideStartBtn();
-
-    // var board = document.createElement('div');
-    // board.className = 'board';
+    hideStartBtn();
+    showHitBtn();
+    showStayBtn();
+    showRestartBtn();
 
     for (var i = 0; i < 2; i++) {
     randomCard = (Math.floor(Math.random() * cardValues.length));
 
-playerCards.push(randomCard);
-
+    playerCards.push(randomCard);
     }
+
     console.log("Player's card is "+playerCards[0]);
     console.log("Player's second card is "+playerCards[1]);
     console.log(playerCards);
@@ -64,7 +132,7 @@ console.log(playerCards);
 var stay = function(event) {
 
 // To sum up the value of playerCards' array.
-        for (let i = 0; i < playerCards.length; i++){
+    for (let i = 0; i < playerCards.length; i++){
             sumOfPlayersCards = sumOfPlayersCards + playerCards[i];
         }
 
@@ -81,5 +149,55 @@ var stay = function(event) {
      }
 }
 
-var winGame = function(){
+var restart = function(event) {
+}
+/////////////////////
+/// THINGS TO ADD ///
+/////////////////////
+
+// var checkWinState = function(){
+
+//    if (sumOfPlayersCards === 21 || playersCards.length >= 5){
+//     alert("YOU WON");
+//    }
+
+//    else if (sumOfPlayersCards < 21 || sumOfPlayersCards > 21)
+//     alert("YOU LOSE :(");
+// }
+
+///////////////////////////////
+///GAME BOARD BUTTON TOGGLES///
+///////////////////////////////
+
+var createBoard = function() {
+for (var i = 0; i < 2; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute("src", "images/hearts/back.png");
+    cardElement.setAttribute("data-id", i);
+    document.getElementById("game-board").appendChild(cardElement);
+    }
+};
+
+var hideStartBtn = function(){
+    var startButton = document.querySelector('#btn-start');
+    document.getElementById("btn-start").style.display = "none";
+    console.log("hide 'START' button");
+}
+
+var showHitBtn = function(){
+    var hitmeButton = document.querySelector('#btn-hit');
+    document.getElementById("btn-hit").style.display = "block";
+    console.log("show 'HIT' button");
+}
+
+var showStayBtn = function(){
+      var startButton = document.querySelector('#btn-stay');
+      document.getElementById("btn-stay").style.display = "block";
+      console.log("show 'STAY' button");
+}
+
+var showRestartBtn = function(){
+    var restartBtn  = document.querySelector('#btn-restart');
+    document.getElementById("btn-restart").style.display = "block";
+    console.log("show 'RESTART' button");
 }
